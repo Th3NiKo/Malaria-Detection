c346e778e6b444f5e0eab2a0c54e0c481707cd67
Th3NiKo <niko.pachala@onet.eu>
2022-07-28 15:09:11 +0200

    Add xception model

--- cnn_training.py
+++ cnn_training.py
@@ -1,6 +1,7 @@
 """
-Basic training pipeline.
+Basic CNN training pipeline.
 """
+import tensorflow as tf
 from tensorflow import keras
 import data_loading
 import model
@@ -9,27 +10,32 @@ EPOCHS = 10
 INITIAL_LEARNING_RATE = 0.0001
 MODEL_NAME = "CNN_BASELINE"
 
-train_data, validation_data, test_data = data_loading.create_image_datasets()
 
-cnn_model = model.create_model()
+def training_pipeline(train_dataset: tf.data.Dataset,
+                      validation_dataset: tf.data.Dataset,
+                      test_dataset: tf.data.Dataset):
+    """Whole training pipeline for basic CNN"""
+    cnn_model = model.create_model()
 
-early_stopping = keras.callbacks.EarlyStopping(patience=3)
+    early_stopping = keras.callbacks.EarlyStopping(patience=3)
+    optimizer = keras.optimizers.Adam(learning_rate=INITIAL_LEARNING_RATE)
 
-optimizer = keras.optimizers.Adam(learning_rate=INITIAL_LEARNING_RATE)
+    cnn_model.compile(
+        loss="binary_crossentropy",
+        optimizer=optimizer,
+        metrics=['accuracy', 'Precision', 'Recall']
+    )
 
-cnn_model.compile(
-    loss="binary_crossentropy",
-    optimizer="adam",
-    metrics=['accuracy', 'Precision', 'Recall']
-)
+    cnn_model.fit(
+        train_dataset,
+        validation_data=validation_dataset,
+        epochs=EPOCHS,
+        callbacks=[early_stopping],
+    )
 
-history = cnn_model.fit(
-    train_data,
-    validation_data=validation_data,
-    epochs=EPOCHS,
-    callbacks=[early_stopping],
-)
+    cnn_model.evaluate(test_dataset)
+    cnn_model.save(MODEL_NAME)
 
-cnn_model.evaluate(test_data)
-
-cnn_model.save(MODEL_NAME)
+if __name__ == "__main__":
+    train_data, validation_data, test_data = data_loading.create_image_datasets()
+    training_pipeline(train_data, validation_data, test_data)
---
+++ xception_transfer_learning.py
@@ -0,0 +1,43 @@
+"""
+Xception model transfer learning.
+"""
+import tensorflow as tf
+# Importing this way because of keras lazy loading autocomplete
+from tensorflow.python.keras.api import keras
+import data_loading
+
+EPOCHS = 3
+LEARNING_RATE = 0.0001
+MODEL_NAME = "XCEPTION"
+
+
+train_data, validation_data, test_data = data_loading.create_image_datasets()
+
+
+def training_pipeline(train_dataset: tf.data.Dataset,
+                      validation_dataset: tf.data.Dataset,
+                      test_dataset: tf.data.Dataset):
+    """Whole training pipeline for xception finetunning"""
+
+    # Create xception model with 1 neuron on output
+    base_model = keras.applications.xception.Xception(weights="imagenet", include_top=False)
+    avg = keras.layers.GlobalAveragePooling2D()(base_model.output)
+    output = keras.layers.Dense(1, activation="sigmoid")(avg)
+    xception_model = keras.Model(inputs=base_model.input, outputs=output)
+
+    early_stopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', mode="max", patience=5)
+
+    optimizer = keras.optimizers.Adam(LEARNING_RATE)
+    loss = keras.losses.BinaryCrossentropy()
+
+    xception_model.compile(loss=loss,
+                           optimizer=optimizer,
+                           metrics=["accuracy","Recall", "Precision"])
+
+    xception_model.fit(train_dataset,
+                       validation_data=validation_dataset,
+                       epochs=EPOCHS,
+                       callbacks=[early_stopping])
+
+    xception_model.evaluate(test_dataset)
+    xception_model.save(MODEL_NAME)
