3ac713bf0a1e955ac9470edfcefdf5fa15e33076
Th3NiKo <niko.pachala@onet.eu>
2022-08-02 13:11:28 +0200

    Xception training into main

--- xception_transfer_learning.py
+++ xception_transfer_learning.py
@@ -11,9 +11,6 @@ LEARNING_RATE = 0.0001
 MODEL_NAME = "XCEPTION"
 
 
-train_data, validation_data, test_data = data_loading.create_image_datasets()
-
-
 def training_pipeline(train_dataset: tf.data.Dataset,
                       validation_dataset: tf.data.Dataset,
                       test_dataset: tf.data.Dataset):
@@ -41,3 +38,7 @@ def training_pipeline(train_dataset: tf.data.Dataset,
 
     xception_model.evaluate(test_dataset)
     xception_model.save(MODEL_NAME)
+
+if __name__ == "__main__":
+    train_data, validation_data, test_data = data_loading.create_image_datasets()
+    training_pipeline(train_data, validation_data, test_data)
